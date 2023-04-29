a15c433dbfed00c946e6d744a5cf24c9816cce0f
Makeshwaran <makesh7969@gmail.com>
2023-04-29 23:04:09 +0530

    Remove url

--- src/components/Footer/LiveChat.js
+++ src/components/Footer/LiveChat.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Script } from "gatsby"
 
 const LiveChat = (props) => {
-    const pageurl = `https://www.petergreatorex.co.uk/`
+    const pageurl = ``
     return (
         <React.Fragment>
             <Script src={`https://clients.yomdel.com/tools/chat_script.js?url=${pageurl}`} strategy="idle" />
