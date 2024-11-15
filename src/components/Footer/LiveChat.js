a89a17b151de963f9a3fc2fb8fac4d00d90e7f43
Makeshwaran <makesh7969@gmail.com>
2024-11-15 11:31:19 +0530

    remove livechat

--- src/components/Footer/LiveChat.js
+++ src/components/Footer/LiveChat.js
@@ -5,7 +5,7 @@ const LiveChat = (props) => {
     const pageurl = ``
     return (
         <React.Fragment>
-            <Script src={`https://clients.yomdel.com/tools/chat_script.js?url=${pageurl}`} strategy="idle" />
+            {/* <Script src={`https://clients.yomdel.com/tools/chat_script.js?url=${pageurl}`} strategy="idle" /> */}
         </React.Fragment>
     )
 }
